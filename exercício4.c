6409a3c67d5693dffaac7463126149e4b0da2037
BeatrizCercal <beatriz_cachoeira@estudante.sesisenai.org.br>
2026-03-16 11:08:05 -0300

    Exercício 4 finalizado

--- exercício4.c
+++ exercício4.c
@@ -2,5 +2,15 @@
 #include <stdlib.h>
 
 int main(void){
-      
+    float temperaturaF;
+    float temperaturaC;
+
+    printf("Digite a temperatura que deseja converter: ");
+    scanf("%f", &temperaturaC);
+
+    temperaturaF = 5 * (temperaturaC - 32) / 9;
+
+    printf("A temperatura em Fahrenheit é: %.2f", temperaturaF);
+
+    return 0;
 }
