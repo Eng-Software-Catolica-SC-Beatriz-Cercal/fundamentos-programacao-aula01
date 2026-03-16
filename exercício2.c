3c195654b0c5138de2d8bfbc6596e848ad6fbf9e
BeatrizCercal <beatriz_cachoeira@estudante.sesisenai.org.br>
2026-03-16 10:53:23 -0300

    Exercício 2 finalizado

---
+++ exercício2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    printf(" ---------------------------------------------\n");
+	printf("| Nome: Beatriz Cercal Cachoeira |\n");
+    printf("| Endereço:Rua Carlos Romualdo do Rosário, 220 |\n");
+    printf("| Telefone (47) 98850-7705 |\n");
+	printf("| Email: biaccachoeira@gmail.com.br |\n");
+    printf(" ---------------------------------------------\n");
+
+
+	return 0;
+}
