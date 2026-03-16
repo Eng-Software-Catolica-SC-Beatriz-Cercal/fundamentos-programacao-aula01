3bff841ff15970b21a4573b975d579e85a63c116
BeatrizCercal <beatriz_cachoeira@estudante.sesisenai.org.br>
2026-03-16 12:34:19 -0300

    Exercício 9 finalizado

---
+++ exercício9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+    float nota1, nota2, nota3, media;
+
+    printf("Digite sua primeira nota: ");
+    scanf("%f", nota1);
+    printf("Digite sua segunda nota: ");
+    scanf("%f", nota2);
+    printf("Digite sua terceira nota: ");
+    scanf("%f", nota3);
+    media = (nota1+nota2+nota3)/3;
+    printf("A média das suas notas é: %.2f", media);
+}
