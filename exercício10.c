6804eb5464642e7cde87e99181b014bd5c03b0b6
BeatrizCercal <beatriz_cachoeira@estudante.sesisenai.org.br>
2026-03-16 12:37:36 -0300

    Exercício 10 finalizado

---
+++ exercício10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+ int main (void){
+    float valor, hora, salario;
+
+printf("Digite o valor que você recebe por hora: ");
+scanf("%f", &valor);
+printf("Digite quantas horas você trabalha: ");
+scanf("%f", &hora);
+salario = valor * hora;
+printf("O valor do seu salário é: %.2f", salario);
+ }
--- exercício9.c
+++ exercício9.c
@@ -5,11 +5,11 @@ int main (void){
     float nota1, nota2, nota3, media;
 
     printf("Digite sua primeira nota: ");
-    scanf("%f", nota1);
+    scanf("%f", &nota1);
     printf("Digite sua segunda nota: ");
-    scanf("%f", nota2);
+    scanf("%f", &nota2);
     printf("Digite sua terceira nota: ");
-    scanf("%f", nota3);
+    scanf("%f", &nota3);
     media = (nota1+nota2+nota3)/3;
     printf("A média das suas notas é: %.2f", media);
 }
