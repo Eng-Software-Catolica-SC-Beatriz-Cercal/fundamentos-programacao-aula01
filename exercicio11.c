1bd686b5829afb6fc3cd9b62401e07ae4159b1fe
BeatrizCercal <beatriz_cachoeira@estudante.sesisenai.org.br>
2026-03-16 21:52:38 -0300

    Exercício 11 finalizado

--- exercicio11.c
+++ exercicio11.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int main (void){
+    float dias, salario, desconto;
+    printf("Quantos dias voce trabalhou?");
+    scanf("%f", &dias);
+    salario = dias * 25;
+    desconto = salario * 0.08;
+    printf("O valor do seu salario, com descontos: %.2f", salario - desconto);
+
+}
