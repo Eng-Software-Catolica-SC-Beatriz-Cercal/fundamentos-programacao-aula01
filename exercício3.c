715328ad6f3e473019cec763ab8b7cbd9d7996ab
BeatrizCercal <beatriz_cachoeira@estudante.sesisenai.org.br>
2026-03-23 19:44:43 -0300

    Exercício 3 comentado

--- exercício3.c
+++ exercício3.c
@@ -2,14 +2,19 @@
 #include <stdlib.h>
 
 int main(void){
+    //Quais são as variáveis?
     float temperaturaF;
     float temperaturaC;
 
+    //Quais são as entradas de dados?
     printf("Digite a temperatura que deseja converter: ");
     scanf("%f", &temperaturaF);
+    fflush(stdin);
 
+    //Processamento de dados;
     temperaturaC = 5 * (temperaturaF - 32) / 9;
 
+    //Quais são as saídas de dados?
     printf("A temperatura em Celsius é: %.2f", temperaturaC);
 
     return 0;
