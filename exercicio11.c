0e6555a5556547927944e74cca5cc6548ae28deb
BeatrizCercal <beatriz_cachoeira@estudante.sesisenai.org.br>
2026-03-23 21:24:04 -0300

    Exercícios 11 e 12 comentados

--- exercicio11.c
+++ exercicio11.c
@@ -2,11 +2,15 @@
 #include <stdlib.h>
 
 int main (void){
+    //Variáveis
     float dias, salario, desconto;
+    //Entrada de dados
     printf("Quantos dias voce trabalhou?");
     scanf("%f", &dias);
+    //Processamento
     salario = dias * 25;
     desconto = salario * 0.08;
+    //Saída de dados
     printf("O valor do seu salario, com descontos: %.2f", salario - desconto);
     return 0;
 
--- exercício12.c
+++ exercício12.c
@@ -2,14 +2,18 @@
 #include <stdlib.h>
 
 int main (void){
+    //Variáveis
     float salariob, ir, inss, sindicato, descontos, salariol;
+    //Entrada de dados
     printf ("Digite seu salaário bruto mensal: R$");
     scanf("%f", &salariob);
+    //Processamento de dados
     ir = salariob * 0.15;
     inss = salariob * 0.11;
     sindicato = salariob * 0.03;
     descontos = ir + inss + sindicato;
     salariol = salariob - descontos;
+    //Saída de dados
     printf("Salário Bruto : R$ %.2f\n", salariob);
     printf ("(-) IR: R$ %.2f\n", ir);
     printf ("(-) INSS: R$ %.2f\n", inss);
